4d1f680c23523608d86f7f2976787bea226b1a46
Adolfo García Vázquez <A01741767@tec.mx>
2024-11-06 17:10:04 -0600

    Se cambio el como esta puesto el require y asi ya obtiene el los valores del .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,23 +1,10 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
+console.log(process.env.CA_CERT);
+console.log(process.env.DB_NAME);
 const mysql = require('mysql2');
-var fs = require("fs");
-const constants = require("../constants");
-const {DB_HOST,DB_PORT,DB_USER,DB_PASS,DB_NAME,CA_CERT} = process.env;
-
-/**
- * Método que configura un objeto conexión y lo regresa a quien lo solicite.
- *
- * Instrucciones:
- *
- * 1. Debes agregar en la carpeta raíz del proyecto un archivo .env donde configures variables de entorno.
- * 2. Configura la información de tu BD en las variables de entorno.
- * 3. Si estás usando Aiven, incluye tambien tu certificado SSL con una ruta válida de tu computadora.
- * 4. En la carpeta database te dejo compartido un archivo .sql para que puedas hacer restore de mi base de datos.
- */
+const fs = require("fs");
 
 function getConnection() {
-
-
   const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -25,12 +12,12 @@ function getConnection() {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     ssl: {
-      ca: CA_CERT //fs.readFileSync('C:\Users\adolf\Desktop\Api_Example_IOT\ca.pem'),
+      ca: fs.readFileSync(process.env.CA_CERT).toString(),
+      rejectUnauthorized: false,
     }
   });
 
   return connection;
 }
 
-console.log("CA:", getConnection.ssl);
 module.exports = { getConnection };
