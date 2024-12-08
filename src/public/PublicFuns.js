f8ba85757fd502038c83329a57d6605833cdec1a
Adolfo García Vázquez <A01741767@tec.mx>
2024-12-08 05:34:59 +0000

    probare esto a ver qpd

--- src/public/PublicFuns.js
+++ src/public/PublicFuns.js
@@ -34,7 +34,7 @@ function crearGrafica() {
     let myChart;
 
     const actualizarGrafica = () => {
-        fetch('http://10.22.198.234:3000/iot/api/getAllDist')
+        fetch('http://io-t-water-tank-monitoring-system.vercel.app/iot/api/getAllDist')
             .then(response => response.json())
             .then(response => {
                 const data = response.data;
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function actualizarTemperatura() {
-    fetch('http://10.22.198.234:3000/iot/api/getallTempe')
+    fetch('http://io-t-water-tank-monitoring-system.vercel.app/iot/api/getallTempe')
         .then(response => response.json())
         .then(response => {
             const data = response.data;
@@ -133,7 +133,7 @@ function actualizarTemperatura() {
 }
 
 function actualizarFotoVal() {
-    fetch('http://10.22.198.234:3000/iot/api/getlastFotovalT5')
+    fetch('http://io-t-water-tank-monitoring-system.vercel.app/iot/api/getlastFotovalT5')
         .then(response => response.json())
         .then(response => {
             const data = response.data;
